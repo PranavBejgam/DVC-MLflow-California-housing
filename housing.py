8b61ccbbfde2373125d1beae33aac6bdfb9e02c7
Pranav Bejgam <pranavbejgam26@gmail.com>
2025-07-18 14:29:30 +0530

    Added raw California housing dataset and tracked with DVC

--- housing.py
+++ housing.py
@@ -1,1 +1,9 @@
-print("hello")
+from sklearn.datasets import fetch_california_housing
+import pandas as pd
+import os
+
+os.makedirs("data", exist_ok=True)
+
+data = fetch_california_housing(as_frame=True)
+df = data.frame
+df.to_csv("data/raw_data.csv", index=False)
